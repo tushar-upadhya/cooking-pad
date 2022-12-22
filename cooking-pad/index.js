1d3972aecbd2fa2bb9dea1956a3720180d7c9fe0
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2022-12-22 15:39:30 +0530

    Update index.js

--- cooking-pad/index.js
+++ cooking-pad/index.js
@@ -1,7 +1,7 @@
 //Initial References
 let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
-let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+let url = "www.themealdb.com/api/json/v1/1/search.php?f=a";
 
 searchBtn.addEventListener("click", () => {
   let userInp = document.getElementById("user-inp").value;
